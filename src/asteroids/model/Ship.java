2a3f2291fd1844c0e1c94fd215ebcc35aea3ac95
jpelgrims <jellepelgrims@hotmail.com>
2017-02-25 14:50:01 +0100

    Added basic code for functions
    The following functions were changed: move, thrust, turn,
    getDistanceBetween & overlap

--- src/asteroids/model/Ship.java
+++ src/asteroids/model/Ship.java
@@ -1,11 +1,13 @@
 package asteroids.model;
-import java.awt.Point;
 
-// Point implementatie in doubles of apart doubles x en y
+import java.awt.geom.Point2D;
 
 public class Ship {
 	
 	/* General comments:
+	 * position: kilometres
+	 * velocity: km/s
+	 * 
 	 * 		- All aspects related to the position of the ship shall be worked out defensively
 	 * 		- All aspects related to velocity must be worked out in a total manner.
 	 * 		- All aspects related to the orientation of the ship must be worked out nominally
@@ -13,32 +15,96 @@ public class Ship {
 	 * 
 	 */
 	
+	private static final double MAX_SPEED = 300000;
+	private static final double MIN_RADIUS = 10;
+
+	private double orientation;
+	private double getOrientation() {return this.orientation;}
+	private void setOrientation(double angle) {this.orientation = angle;}
+	
+	private double radius;
+	private double getRadius() {return this.radius;}
+	private void setRadius(double radius) {this.radius = radius;}
+	
+	private Point2D.Double position;
+	private Point2D.Double velocity;
+	
+	public boolean isValidDuration (double duration) {
+		return duration >= 0;
+	}
+	
 	public Ship(double x, double y, double xVelocity, double yVelocity, double radius, double orientation) {
+		
+		position.setLocation(x, y);
+		velocity.setLocation(xVelocity, yVelocity);
+		setRadius(radius); // larger than 10 km
+		setOrientation(orientation); //between 0 and 2pi
 		
 	};
 	
-	private static final double MAX_SPEED = 300000;
+	/**
+	 * 
+	 * @param duration
+	 */
+	public void move(double duration) throws Exception {
+		// Implement defensively
+		if (isValidDuration(duration)) {
+			double deltaX = velocity.getX()*duration;
+			double deltaY = velocity.getY()*duration;
+			position.setLocation(position.getX()+deltaX, position.getY()+deltaY);
+		} else {
+			throw new Exception();
+		}
+		
+	}
 	
-	private Point position;
-	public Point getPosition(){return this.position;}
-	private void setPosition(Point position) {this.position = position;}
+	public boolean isValidAngle(double angle) {
+		return true;
+	}
 	
-	private Point velocity;
-	public Point getVelocity(){return this.velocity;}
-	private void setVelocity(Point velocity) {this.velocity = velocity;}
+	public void turn(double angle) {
+		// Implement nominally
+		double radians = Math.toRadians(angle);
+		setOrientation(this.orientation+radians % 2*Math.PI);
+	}
+	public void thrust(double amount) {
+		// Implement totally
+		if (amount < 0) {
+			amount = 0;
+		}
+		double newVelocityX = velocity.getX() + amount*Math.cos(getOrientation());
+		double newVelocityY = velocity.getY() + amount*Math.sin(getOrientation());
+		double newMagnitude = Math.sqrt(Math.pow(newVelocityX, 2)+Math.pow(newVelocityY, 2));
+		
+		if (newMagnitude > MAX_SPEED) {
+			// reduce Vx and Vy without changing direction until magnitude < c
+		}
+				
+		velocity.setLocation(newVelocityX, newVelocityY);
+		
+	}
 	
-	public void move(double duration) {}
-	public void turn(double angle) {}
-	public void thrust(double thrust) {}
-	
-	public double getDistanceBetween(Ship other) {return null;}
-	public boolean overlap(Ship other) {return false;}
-	public double getTimeToCollision(Ship other) {return null;}
-	public Point getCollisionPosition(Ship other) {return null;}
+	public double getDistanceBetween(Ship other) throws Exception {
+		if (other == this) {
+			return new Double(0);
+		} else {
+			double distance = Math.sqrt(Math.pow((other.position.getX()-this.position.getX()), 2)+Math.pow((other.position.getY()-this.position.getY()), 2));
+			return distance;
+		}
+	}
+
+	public boolean overlap(Ship other) throws Exception {
+		// see if the distance between the centers of the two circles exceeds r1+r2. Then they cannot overlap in the plane. 
+		// r1+r2 < sqrt( (x2-x1)**2 + (y2-y1)**2 ) 	
+		double radiusSum = this.getRadius() + other.getRadius();
+		double distance = Math.sqrt(Math.pow((other.position.getX()-this.position.getX()), 2)+Math.pow((other.position.getY()-this.position.getY()), 2));
+		return radiusSum < distance;
+
+	}
+	public double getTimeToCollision(Ship other) throws Exception {return null;}
+	public Point2D.Double getCollisionPosition(Ship other) throws Exception {return null;}
 	
 	
 	
 
 }
-
-
